f7034e5d28a01af004c5d1752edd8647d0c5004b
Alnoman Eihab Kamil <alneihzen@gmail.com>
2021-03-02 16:59:24 +0200

    Add files via upload

---
+++ ID6.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Sum square difference
+
+Problem 6
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+"""
+
+sum_of_squares = sum([i**2 for i in range(1,101)])
+square_of_sum = sum([i for i in range(1,101)])**2
+
+print(square_of_sum - sum_of_squares)
