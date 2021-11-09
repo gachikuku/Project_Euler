946d1cc20f57a5afbfc100139c20d4f21ed08c8c
Alnoman Kamil <alneihzen@gmail.com>
2021-11-09 02:58:23 +0200

    Add files via upload

--- problem31_v2.cpp
+++ problem31_v2.cpp
@@ -30,8 +30,12 @@ int main(){
     ios::sync_with_stdio(0);
     
     cin.tie(0);
+    
+    int N;
+    
+    cin >> N;
 
-    cout << findposs(200, 0) << "\n";
+    cout << findposs(N, 0) << "\n";
     
     return 0;
 }
